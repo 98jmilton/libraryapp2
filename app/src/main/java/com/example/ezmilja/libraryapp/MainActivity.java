f14294578fe9ee3e1e735277ecfce0d6b8c1f66c
Gretzy96 <seangrenham3g@gmail.com>
2018-07-16 11:20:23 +0100

    WIZARDS

--- app/src/main/java/com/example/ezmilja/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/MainActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -31,11 +35,43 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void run() {
 
-                                // Write a message to the database
                                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                                DatabaseReference myRef = database.getReference("Greeting");
+                                DatabaseReference BookRef = database.getReference("Books");
+                                BookRef.getParent().push();
 
-                                myRef.setValue("edfedd");
+//                                myRef.child("Book A").push().setValue(1);
+//                                myRef.child("Book A").push().setValue(53);
+//                                myRef.child("Book A").push().setValue(42);
+//
+//                                myRef.child("Book B").push().setValue("Harry plopper the beginning of biscuits ");
+//                                myRef.child("Book B").push().setValue("Harry plopper and the swag stone");
+                                BookRef.child("Wizard").push().setValue("id","Harry plopper and the boys from uganda");
+
+
+
+                                BookRef.child("Book C").push().setValue("a");
+                                BookRef.child("Book C").push().setValue("z");
+                                BookRef.child("Book C").push().setValue("c");
+
+
+
+//                                BookRef.addValueEventListener(new ValueEventListener() {
+//                                    @Override
+//                                    public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                                        for (DataSnapshot chidSnap : dataSnapshot.getChildren()) {
+//                                            Log.v("tmz",""+ chidSnap.getKey()); //displays the key for the node
+//                                          // Log.v("tmz",""+ chidSnap.child("Book A").getValue());   //gives the value for given keyname
+//                                        }
+//
+//                                    }
+//
+//                                    @Override
+//                                    public void onCancelled(DatabaseError databaseError) {
+//
+//                                    }
+//                                });
+
 
                                 Intent intent = new Intent(MainActivity.this, ContentsActivity.class);
                                 finish();
