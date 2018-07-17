e672efa0983dc2d4246108f2c9b1aba838a2da1b
James Milton <98jmilton@gmail.com>
2018-07-17 10:37:11 +0100

    Gutted fish 2

--- app/src/main/java/com/example/ezmilja/libraryapp/ContentsActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/ContentsActivity.java
@@ -2,18 +2,11 @@ package com.example.ezmilja.libraryapp;
 
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class ContentsActivity extends AppCompatActivity {
 
@@ -21,11 +14,6 @@ public class ContentsActivity extends AppCompatActivity {
     private Button btn31list;
     private Button btn_rqst;
     private Button btn_check;
-
-
-    // Write a message to the database
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("Greeting");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,29 +42,9 @@ public class ContentsActivity extends AppCompatActivity {
         TextView textView2 = (TextView) findViewById(R.id.textView2);
         textView2.setTypeface(myTypeFace1);
 
-        btn_list = (Button) findViewById(R.id.btn31list);
         btn_list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                // Read from the database
-                myRef.addValueEventListener(new ValueEventListener() {
-                    public static final String TAG = "";
-
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        // This method is called once with the initial value and again
-                        // whenever data at this location is updated.
-                        String value = dataSnapshot.getValue(String.class);
-                        Log.d(TAG, "Value is: " + value);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError error) {
-                        // Failed to read value
-                        Log.w(TAG, "Failed to read value.", error.toException());
-                    }
-                });
                 Intent intent = new Intent(ContentsActivity.this, ListActivity.class);
                 startActivity(intent);
             }
---
+++ app/src/main/java/com/example/ezmilja/libraryapp/SplashScreen.java
@@ -0,0 +1,138 @@
+package com.example.ezmilja.libraryapp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Typeface myTypeFace1 = Typeface.createFromAsset(getAssets(), "yourfont.ttf");
+        TextView TextView1 = (TextView) findViewById(R.id.TextView1);
+        TextView1.setTypeface(myTypeFace1);
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference BookRef = database.getReference("Books");
+
+        BookRef.child("Book1").child("Title").setValue("Paul");
+        BookRef.child("Book1").child("Author").setValue("do you mean");
+        BookRef.child("Book1").child("ISBN").setValue("are you going");
+        BookRef.child("Book2").child("Title").setValue("Sean");
+        BookRef.child("Book2").child("Author").setValue("are you doing");
+        BookRef.child("Book2").child("ISBN").setValue("are you done");
+
+
+        requestPermission();
+
+
+    }
+
+    private void requestPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat
+                    .requestPermissions(SplashScreen.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_ASK_PERMISSIONS);
+        }
+        else{
+
+            Thread myThread = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        sleep(2000);
+                        Intent intent = new Intent(getApplicationContext(),ContentsActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            myThread.start();
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    // Permission Granted
+                    Toast.makeText(SplashScreen.this, "Permission Granted", Toast.LENGTH_SHORT)
+                            .show();
+
+                    Thread myThread = new Thread() {
+                        @Override
+                        public void run() {
+                            try {
+                                sleep(2000);
+                                Intent intent = new Intent(getApplicationContext(),
+                                        ContentsActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+                    myThread.start();
+
+                }
+
+                else
+                {
+                    // Permission Denied
+                    Toast.makeText(SplashScreen.this, "Permission Denied", Toast.LENGTH_SHORT)
+                            .show();
+
+                    Thread myThread = new Thread() {
+                        @Override
+                        public void run() {
+                            try {
+                                sleep(2000);
+                                Intent intent = new Intent(getApplicationContext(),ContentsActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+                    myThread.start();
+
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
+
