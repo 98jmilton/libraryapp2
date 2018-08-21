3e72f53b39d00457b89569b1705797c560fc9f2d
Gretzy96 <seangrenham3g@gmail.com>
2018-08-21 16:13:20 +0100

    added change password functionality

--- app/src/main/java/com/example/ezmilja/libraryapp/ContentsActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/ContentsActivity.java
@@ -2,13 +2,21 @@ package com.example.ezmilja.libraryapp;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static com.example.ezmilja.libraryapp.SplashScreen.j;
 
@@ -48,10 +56,6 @@ public class ContentsActivity extends AppCompatActivity {
         Button btn_check = findViewById(R.id.btn_check);
         btn_check.setTypeface(myTypeFace1);
 
-        //logout of the firebase authentication
-        Button btn_logout = findViewById(R.id.btn_logout);
-        btn_logout.setTypeface(myTypeFace1);
-
 
         TextView textView2 = findViewById(R.id.textView2);
         textView2.setTypeface(myTypeFace1);
@@ -83,17 +87,55 @@ public class ContentsActivity extends AppCompatActivity {
             }
         });
 
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbarmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout_menu:
                 //logging out the user
                 firebaseAuth.signOut();
                 //closing activity
                 finish();
                 //starting login activity
                 startActivity(new Intent(ContentsActivity.this, LoginActivity.class));
-            }
-        });
+                return true;
+
+            case R.id.changepassword_menu:
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+
+                String emailAddress = user.getEmail();
+
+                auth.sendPasswordResetEmail(emailAddress)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    // do something when mail was sent successfully.
+                                    Toast.makeText(ContentsActivity.this, "An password reset has been sent to your email", Toast.LENGTH_SHORT).show(); return;
+
+                                } else {
+                                    Toast.makeText(ContentsActivity.this, "Failed to send password reset email", Toast.LENGTH_SHORT).show(); return;
+                                    }
+                            }
+                        });
+                return true;
+
+            default:
+
+
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
--- app/src/main/java/com/example/ezmilja/libraryapp/LoginActivity.java
+++ app/src/main/java/com/example/ezmilja/libraryapp/LoginActivity.java
@@ -1,11 +1,17 @@
 package com.example.ezmilja.libraryapp;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +38,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     //progress dialog
     private ProgressDialog progressDialog;
+
+
+    public String email_popupText = "";
 
 
     @Override
@@ -143,6 +152,82 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             // Signup button is clicked
             finish();
             startActivity(new Intent(this, SignUp.class));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.forgotpassword_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.changepassword_menu:
+
+
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Please enter your email here to send password reset");
+
+                // Set up the input
+                final EditText input = new EditText(this);
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                input.setInputType(InputType.TYPE_CLASS_TEXT );  // | InputType.TYPE_TEXT_VARIATION_PASSWORD
+                builder.setView(input);
+
+                // Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        email_popupText = input.getText().toString();
+                        if (!email_popupText.endsWith("@ericsson.com")){
+                            Toast.makeText(LoginActivity.this, "Not an Ericsson email", Toast.LENGTH_SHORT).show();
+
+                        }
+                        else
+                            {
+
+                            FirebaseAuth auth = FirebaseAuth.getInstance();
+
+                            auth.sendPasswordResetEmail(email_popupText)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                // do something when mail was sent successfully.
+                                                Toast.makeText(LoginActivity.this, "Password reset has been sent to your email", Toast.LENGTH_SHORT).show();
+
+                                            } else {
+                                                // ...
+                                                Toast.makeText(LoginActivity.this, "Failed to send password reset email", Toast.LENGTH_SHORT).show();
+
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+
+
+
+                return true;
+
+            default:
+
+
+                return super.onOptionsItemSelected(item);
         }
     }
 
